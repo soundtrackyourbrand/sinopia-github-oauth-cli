7c9e6c7bb5dcc42292c720bc285f273ceed5b4e7
Fredrik Enestad <fredrik.enestad@soundtrackyourbrand.com>
2016-04-14 23:08:46 +0200

    add all the things

---
+++ index.js
@@ -0,0 +1,24 @@
+var opener = require('opener');
+var http = require('http');
+var url = require('url');
+var exec = require('child_process').execSync;
+
+var registry = exec('npm config get registry').toString().trim()
+
+opener(registry + 'oauth/authorize');
+
+http.createServer(function(req, res) {
+  var query = url.parse(req.url, true).query;
+  var token = decodeURIComponent(query.token);
+
+  var u = url.parse(registry);
+  var keyBase = '//' + u.host + u.path + ':'
+  exec('npm config set ' + keyBase + '_authToken "' + token + '"');
+  exec('npm config set ' + keyBase + 'always-auth true');
+
+  res.write('DONE! Get back you your terminal');
+  res.end();
+
+  process.exit(0);
+
+}).listen(8239);
